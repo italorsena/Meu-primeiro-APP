a7ac9f274c625275d7cdce307e801510d41b340c
italorsena <italosenacvo@gmail.com>
2022-04-15 14:00:09 -0300

    criando funções de adicionar e remover o produto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import addIcon from "./icons/plus-solid.svg"
+import removeIcon from "./icons/remove.svg"
 
 function App() {
+    let lista:string[] = ["Óculos", "Limpa Lentes"]
+    const [carrinho, setCarrinho] = useState <string[]>([])
+    const adicionaProduto = (produto:string) => {
+        setCarrinho([...carrinho,produto])
+        console.log(carrinho)
+    }
+    const removeProduto = (produto:string) => {
+        let indice = carrinho.findIndex((e) => { return e === produto})
+    }
     return (
         <div>
-        
+            <h1>Lista de Produtos Disponiveis</h1>
+            <ul>
+                {lista.map((produto) => { return (
+                    <div>
+                        <li>{produto}</li> 
+                        <button onClick={() => adicionaProduto(produto)}><img src={addIcon} alt="adicionar" /></button>
+                    </div>
+                ) })}
+            </ul>
+            
+            <h1>Carrinho</h1>
+            <ul>
+                {carrinho.map((produto) => {return ( 
+                    <div>
+                        <li>{produto}</li>
+                        <button onClick={() => removeProduto(produto)}><img src={removeIcon} alt="adicionar" /></button>
+
+                    </div>
+                ) })}
+                
+            </ul>
         </div>
     );
 }
